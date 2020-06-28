f3529145dbe56aba5e7593465767f2b9ee937901
tangyican <1371187804@qq.com>
2020-06-28 23:55:25 +0800

    jd

---
+++ JD/index.js
@@ -0,0 +1,4 @@
+$("#nav").load("/nav/index.html");
+$("#search").load("/search/index.html");
+$("#show").load("/show/index.html");
+$("#shortcut").load("/shortcut/index.html");
---
+++ JD/shortcut/index.js
@@ -0,0 +1,12 @@
+$("#shortcutListWrapper").swiper({
+    animateType: "animation",
+    isAutoChange: true,
+    domList: $("#shortcutListWrapper > .item"),
+    showChangeBtn: "always",
+    showSportBtn: false,
+    sportBtnPosition: "left",
+    width: 800,
+    height: 260,
+    showIndex: 0,
+    autoTime: 3500,
+})
